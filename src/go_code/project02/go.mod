452e462be6218b3355fbb2cac64959f762ac199a
wangju <wangjuchn@outlook.com>
2022-11-06 18:57:33 +0800

    项目结构重置

--- src/go_code/project02/go.mod
+++
@@ -1,3 +0,0 @@
-module project02
-
-go 1.19
--- src/project02/main/main.go
+++ src/project02/main/main.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: wangju wangjuchn@outlook.com
+ * @Date: 2022-11-06 18:20:12
+ * @LastEditors: wangju wangjuchn@outlook.com
+ * @LastEditTime: 2022-11-06 18:55:55
+ * @FilePath: /src/project02/main/main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package main
 
 import (
