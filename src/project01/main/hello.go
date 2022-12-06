724cc3326b43fcbd911c11b59925bd03ee351d61
wangju <wangjuchn@outlook.com>
2022-12-06 15:46:01 +0800

    单元测试

--- src/project01/main/hello.go
+++ src/project01/main/hello.go
@@ -2,8 +2,9 @@
 package main
 
 import "fmt"
+
 func main() {
 	fmt.Println("hello world!")
 	// 定义一个变量，如果你没有使用这个变量，就会编译通不过
-	var num = 10
+	// var num = 10
 }
---
+++ src/project02/model/model_test.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHeroname(t *testing.T) {
+	fmt.Println(Heroname)
+}
+
+func TestDbConnect(t *testing.T) {
+	DbConnect()
+}
